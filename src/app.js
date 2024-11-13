5797ae8d9f53b37ca2f26eb00475e87096d91270
redjedi1017 <47050050+redjedi1017@users.noreply.github.com>
2024-11-13 17:04:58 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["My dog", "My confused grandma", "The evil mailman", "My alligator"];
+let action = ["destroyed", "peed on", "crushed", "annihilated"];
+let what = ["my homework", "my phone", "my car"];
+let when = ["before class", "while I was sleeping", "while I was exercising", "during lunch", "While I was gaming"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+    const subject = who[Math.floor(Math.random() * who.length)];
+    const verb = action[Math.floor(Math.random() * action.length)];
+    const object = what[Math.floor(Math.random() * what.length)];
+    const time = when[Math.floor(Math.random() * when.length)];
+    
+    return `${subject} ${verb} ${object} ${time}`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+    document.getElementById("excuse").innerHTML = generateExcuse();
 };
