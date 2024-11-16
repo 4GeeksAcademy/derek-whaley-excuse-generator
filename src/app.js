c7c94d6328c1a0b997e43f7239c9a7141ff05bcf
redjedi1017 <47050050+redjedi1017@users.noreply.github.com>
2024-11-16 01:17:48 +0000

    Changed Stylesheet and HTML

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 let who = ["My dog", "My confused grandma", "The evil mailman", "My alligator"];
 let action = ["destroyed", "peed on", "crushed", "annihilated"];
 let what = ["my homework", "my phone", "my car"];
-let when = ["before class", "while I was sleeping", "while I was exercising", "during lunch", "While I was gaming"];
+let when = [
+  "before class",
+  "while I was sleeping",
+  "while I was exercising",
+  "during lunch",
+  "while I was gaming"
+];
 
 function generateExcuse() {
-    const subject = who[Math.floor(Math.random() * who.length)];
-    const verb = action[Math.floor(Math.random() * action.length)];
-    const object = what[Math.floor(Math.random() * what.length)];
-    const time = when[Math.floor(Math.random() * when.length)];
-    
-    return `${subject} ${verb} ${object} ${time}`;
+  const subject = who[Math.floor(Math.random() * who.length)];
+  const verb = action[Math.floor(Math.random() * action.length)];
+  const object = what[Math.floor(Math.random() * what.length)];
+  const time = when[Math.floor(Math.random() * when.length)];
+
+  return `${subject} ${verb} ${object} ${time}`;
 }
 
 window.onload = () => {
-    document.getElementById("excuse").innerHTML = generateExcuse();
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
